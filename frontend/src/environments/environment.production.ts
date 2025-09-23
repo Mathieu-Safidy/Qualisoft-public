b65e31edc6ffcce7a9ecfff4538a613887c84ae3
Mathieu RAKOTOARISOA <148275593+Mathieu-Safidy@users.noreply.github.com>
2025-09-23 14:33:00 +0300

    Fix environnement

--- frontend/src/environments/environment.production.ts
+++ frontend/src/environments/environment.production.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: true,
     apiBack: '/qualisoft',
+    tableDeplacement: ['projet','interlocuteur','etape_qualite', 'type_erreur', 'erreur_suggestion'],
 };
